b9f6a7931ff050345d806ea58cdc4d85e3d41908
jiangyanfeng <jiangyanfeng@bigo.sg>
2019-02-02 12:08:14 +0800

    allow leading zeros in length

--- message/asn1.go
+++ message/asn1.go
@@ -632,11 +632,11 @@ func parseTagAndLength(bytes []byte, initOffset int) (ret TagAndLength, offset i
 			}
 			ret.Length <<= 8
 			ret.Length |= int(b)
-			if ret.Length == 0 {
-				// DER requires that lengths be minimal.
-				err = StructuralError{"superfluous leading zeros in length"}
-				return
-			}
+			//if ret.Length == 0 {
+			//	// DER requires that lengths be minimal.
+			//	err = StructuralError{"superfluous leading zeros in length"}
+			//	return
+			//}
 		}
 	}
 
